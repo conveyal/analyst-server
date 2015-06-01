8749f9cfc802cf231a38971eff8fbf45de02d7cb
Matthew Wigginton Conway <matt@indicatrix.org>
2015-06-01 10:27:35 -0400

    fix crash with scenarios that have not started.

--- app/Global.java
+++ app/Global.java
@@ -1,14 +1,11 @@
-import java.io.IOException;
-
 import models.Bundle;
 import models.Query;
 import models.Shapefile;
-import models.User;
-import controllers.Api;
 import play.Application;
 import play.GlobalSettings;
-import play.api.mvc.EssentialFilter;
 import utils.QueryResultStore;
+
+import java.io.IOException;
 
 public class Global extends GlobalSettings {
   
@@ -26,7 +23,7 @@ public class Global extends GlobalSettings {
 		}
 
 		for (Query q : Query.getAll()) {
-			if (!q.completePoints.equals(q.totalPoints)) {
+			if (q.completePoints == null || !q.completePoints.equals(q.totalPoints)) {
 				QueryResultStore.accumulate(q);
 			}
 		}
