c2b885c9839479c4366cb5a08cf1d582153ebcee
Matthew Wigginton Conway <matt@indicatrix.org>
2015-06-09 16:07:01 -0400

    allow CORS to queryBins.

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -6,16 +6,16 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
-import models.*;
+import models.Bundle;
+import models.Project;
+import models.Shapefile;
+import models.User;
 import org.joda.time.LocalDate;
 import otp.Analyst;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
-import utils.QueryResults;
-import utils.ResultEnvelope;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -115,83 +115,6 @@ public class Api extends Controller {
     	
     	// we don't have a date to return that is valid in all feeds
     	return ok(originalDate.toString());*/
-    }
-
-    public static Result queryBins(String queryId, Integer timeLimit, String weightByShapefile, String weightByAttribute, String groupBy,
-    		String which, String attributeName, String compareTo) {
-    	
-		response().setHeader(CACHE_CONTROL, "no-cache, no-store, must-revalidate");
-		response().setHeader(PRAGMA, "no-cache");
-		response().setHeader(EXPIRES, "0");
-
-		ResultEnvelope.Which whichEnum;
-		try {
-			whichEnum = ResultEnvelope.Which.valueOf(which);
-		} catch (Exception e) {
-			// no need to pollute the console with a stack trace
-			return badRequest("Invalid value for which parameter");
-		}
-
-		Query query = Query.getQuery(queryId);
-
-		if(query == null)
-			return badRequest();
-		Query otherQuery = null;
-		
-		if (compareTo != null) {
-			otherQuery = Query.getQuery(compareTo);
-			
-			if (otherQuery == null) {
-				return badRequest("Non-existent comparison query.");
-			}
-		}
-	   	
-    	try {
-
-    		String queryKey = queryId + "_" + timeLimit + "_" + which + "_" + attributeName;
-
-    		QueryResults qr = null;
-
-    		synchronized(QueryResults.queryResultsCache) {
-    			if(!QueryResults.queryResultsCache.containsKey(queryKey)) {
-	    			qr = new QueryResults(query, timeLimit, whichEnum, attributeName);
-	    			QueryResults.queryResultsCache.put(queryKey, qr);
-	    		}
-	    		else
-	    			qr = QueryResults.queryResultsCache.get(queryKey);
-    		}
-    		
-    		if (otherQuery != null) {
-        		QueryResults otherQr = null;
-        		
-    			queryKey = compareTo + "_" + timeLimit + "_" + which + "_" + attributeName;
-    			if (!QueryResults.queryResultsCache.containsKey(queryKey)) {
-    				otherQr = new QueryResults(otherQuery, timeLimit, whichEnum, attributeName);
-    				QueryResults.queryResultsCache.put(queryKey, otherQr);
-    			}
-    			else {
-    				otherQr = QueryResults.queryResultsCache.get(queryKey);
-    			}
-    			
-    			qr = qr.subtract(otherQr);
-    		}
-
-            if(weightByShapefile == null) {
-            	return ok(Json.toJson(qr.classifier.getBins()));
-            }
-            else {
-            	Shapefile aggregateTo = Shapefile.getShapefile(groupBy);
-
-				Shapefile weightBy = Shapefile.getShapefile(weightByShapefile);
-				return ok(Json.toJson(qr.aggregate(aggregateTo, weightBy, weightByAttribute).classifier.getBins()));
-	        
-            }
-
-
-    	} catch (Exception e) {
-	    	e.printStackTrace();
-	    	return badRequest();
-	    }
     }
 
 
--- app/controllers/QueryController.java
+++ app/controllers/QueryController.java
@@ -1,24 +1,30 @@
 package controllers;
 
 import models.Query;
+import models.Shapefile;
+import play.Play;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import utils.QueryResults;
+import utils.ResultEnvelope;
 
 import java.io.IOException;
 
 /**
  * Multipoint query controller
  */
-@Security.Authenticated(Secured.class)
 public class QueryController extends Controller {
 
     // **** query controllers ****
 
+    @Security.Authenticated(Secured.class)
     public static Result getQueryById(String id) {
         return getQuery(id, null, null);
     }
 
+    @Security.Authenticated(Secured.class)
     public static Result getQuery(String id, String projectId, String pointSetId) {
 
         try {
@@ -43,6 +49,7 @@ public class QueryController extends Controller {
 
     }
 
+    @Security.Authenticated(Secured.class)
     public static Result createQuery() throws IOException {
 
         Query  q = Query.create();
@@ -57,6 +64,7 @@ public class QueryController extends Controller {
 
     }
 
+    @Security.Authenticated(Secured.class)
     public static Result updateQuery(String id) {
 
         Query q;
@@ -77,6 +85,7 @@ public class QueryController extends Controller {
         }
     }
 
+    @Security.Authenticated(Secured.class)
     public static Result deleteQuery(String id) throws IOException {
         if(id == null)
             return badRequest();
@@ -89,5 +98,88 @@ public class QueryController extends Controller {
         q.delete();
 
         return ok();
+    }
+
+    /** Get the classes/bins for a particular query */
+    public static Result queryBins(String queryId, Integer timeLimit, String weightByShapefile, String weightByAttribute, String groupBy,
+                                   String which, String attributeName, String compareTo) {
+
+        if (session().get("username") == null &&
+                Play.application().configuration().getBoolean("api.allow-unauthenticated-access") != true)
+            return unauthorized();
+
+        // allow cross-origin access if we don't need auth
+        if (Play.application().configuration().getBoolean("api.allow-unauthenticated-access") == true)
+            response().setHeader("Access-Control-Allow-Origin", "*");
+
+        response().setHeader(CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        response().setHeader(PRAGMA, "no-cache");
+        response().setHeader(EXPIRES, "0");
+
+        ResultEnvelope.Which whichEnum;
+        try {
+            whichEnum = ResultEnvelope.Which.valueOf(which);
+        } catch (Exception e) {
+            // no need to pollute the console with a stack trace
+            return badRequest("Invalid value for which parameter");
+        }
+
+        Query query = Query.getQuery(queryId);
+
+        if (query == null)
+            return badRequest();
+        Query otherQuery = null;
+
+        if (compareTo != null) {
+            otherQuery = Query.getQuery(compareTo);
+
+            if (otherQuery == null) {
+                return badRequest("Non-existent comparison query.");
+            }
+        }
+
+        try {
+
+            String queryKey = queryId + "_" + timeLimit + "_" + which + "_" + attributeName;
+
+            QueryResults qr = null;
+
+            synchronized (QueryResults.queryResultsCache) {
+                if (!QueryResults.queryResultsCache.containsKey(queryKey)) {
+                    qr = new QueryResults(query, timeLimit, whichEnum, attributeName);
+                    QueryResults.queryResultsCache.put(queryKey, qr);
+                } else
+                    qr = QueryResults.queryResultsCache.get(queryKey);
+            }
+
+            if (otherQuery != null) {
+                QueryResults otherQr = null;
+
+                queryKey = compareTo + "_" + timeLimit + "_" + which + "_" + attributeName;
+                if (!QueryResults.queryResultsCache.containsKey(queryKey)) {
+                    otherQr = new QueryResults(otherQuery, timeLimit, whichEnum, attributeName);
+                    QueryResults.queryResultsCache.put(queryKey, otherQr);
+                } else {
+                    otherQr = QueryResults.queryResultsCache.get(queryKey);
+                }
+
+                qr = qr.subtract(otherQr);
+            }
+
+            if (weightByShapefile == null) {
+                return ok(Json.toJson(qr.classifier.getBins()));
+            } else {
+                Shapefile aggregateTo = Shapefile.getShapefile(groupBy);
+
+                Shapefile weightBy = Shapefile.getShapefile(weightByShapefile);
+                return ok(Json.toJson(qr.aggregate(aggregateTo, weightBy, weightByAttribute).classifier.getBins()));
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return badRequest();
+        }
     }
 }
