549a577d0ae3a96fa51e2595f4ba7ba5d0043d44
Matthew Wigginton Conway <matt@indicatrix.org>
2015-06-24 16:12:49 -0400

    get geom2gtfs working again.

--- src/main/java/com/conveyal/analyst/server/jobs/Geom2GtfsJob.java
+++ src/main/java/com/conveyal/analyst/server/jobs/Geom2GtfsJob.java
@@ -1,14 +1,11 @@
 package com.conveyal.analyst.server.jobs;
 
-import com.conveyal.analyst.server.AnalystMain;
+import com.conveyal.geom2gtfs.Main;
 import models.Bundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Run geom2gtfs with the given shapefile and config file for the given scenario.
@@ -32,30 +29,19 @@ public class Geom2GtfsJob implements Runnable {
 	
 	@Override
 	public void run() {
-		try {			
-			// overwrite
-			if (newFile.exists())
-				newFile.delete();
-	
-			ProcessBuilder pb = new ProcessBuilder("java"
-					,"-jar",new File(AnalystMain.config.getProperty("application.bin"), "geom2gtfs.jar").getAbsolutePath(),  shapeFile.getAbsolutePath(), configFile.getAbsolutePath(), newFile.getAbsolutePath());
-			// show messages from stderr as well
-			pb.redirectErrorStream(true);
-			Process p;
-			p = pb.start();
-	
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-	
-			while((line = bufferedReader.readLine()) != null){
-				LOG.info("geom2gtfs: " + line);
-			}
-	
-			LOG.info("shapefile " + shapeFile.getName() + " processed.");
-		} catch (IOException e) {
-			// rethrow as unchecked
-			throw new RuntimeException(e);
-		}
-	}
+		// overwrite
+		if (newFile.exists())
+			newFile.delete();
 
+		// this is cloogy - we're calling a Java main function from inside a Java program
+		try {
+			Main.main(new String[] { shapeFile.getAbsolutePath(), configFile.getAbsolutePath(),
+					newFile.getAbsolutePath() });
+		} catch (Exception e) {
+			LOG.error("geom2gtfs error", e);
+			return;
+		}
+
+		LOG.info("shapefile " + shapeFile.getName() + " processed.");
+	}
 }
