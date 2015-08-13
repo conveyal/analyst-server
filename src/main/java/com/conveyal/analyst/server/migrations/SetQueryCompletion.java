133354e665b1a03dbb5e579ed44c89545e05d39c
Matthew Wigginton Conway <matt@indicatrix.org>
2015-08-13 09:08:02 -0400

    set the complete flag based on the complete points.

---
+++ src/main/java/com/conveyal/analyst/server/migrations/SetQueryCompletion.java
@@ -0,0 +1,28 @@
+package com.conveyal.analyst.server.migrations;
+
+import com.conveyal.analyst.server.utils.DataStore;
+import models.Query;
+
+import java.io.File;
+
+/**
+ * Update the query complete flag based on whether completePoints equals totalPoints.
+ */
+public class SetQueryCompletion {
+    public static void main (String... args) {
+        File input = new File(args[0]);
+
+        if (!input.exists() || !input.isDirectory()) {
+            System.err.println("Input is not a directory or does not exist.");
+            return;
+        }
+
+        DataStore<Query> queryStore = new DataStore<Query>(input, "queries");
+
+        queryStore.getAll().stream().filter(q -> q.totalPoints != null && q.totalPoints.equals(q.completePoints))
+                .forEach(q -> {
+                    q.complete = true;
+                    q.save();
+                });
+    }
+}
