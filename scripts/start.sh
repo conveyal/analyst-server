3b159eae4671cb24cead20c4d50021e9a3d4d8d6
Matthew Wigginton Conway <matt@indicatrix.org>
2015-08-13 11:42:50 -0400

    autodetect heap size.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# figure out how much memory to use. Stats get 512M, broker gets 1G, OS gets 1G, analyst gets rest
+TOTAL_MEM=`grep MemTotal /proc/meminfo | sed s/[^0-9]//g`
+ANALYST_MEM=`echo "$TOTAL_MEM - (2500 * 1024)" | bc`
+
 # start analyst server, detaching it from the terminal
 # One would think that the nohup command should do this but it doesn't detach standard error
 cd /ebs/scratch
-java -Xmx8G -Dlogback.configurationFile=/etc/analyst-logentries.xml -jar /opt/otp/analyst-server.jar /etc/analyst.conf > /home/ubuntu/analyst.log < /dev/null 2>&1 &
+java -Xmx${ANALYST_MEM}k -Dlogback.configurationFile=/etc/analyst-logentries.xml -jar /opt/otp/analyst-server.jar /etc/analyst.conf > /home/ubuntu/analyst.log < /dev/null 2>&1 &
 echo $! > /tmp/ANALYST_PID
 
 # start the broker. conveniently enough we can use the analyst server jar, which just happens to already have the
