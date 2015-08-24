5932c2d04d52af2c89209c2de92510f279476ce8
Matthew Wigginton Conway <matt@indicatrix.org>
2015-08-24 09:42:25 -0400

    add unit test for quota ledger.

--- src/test/java/com/conveyal/analyst/server/utils/QuotaLedgerTest.java
+++ src/test/java/com/conveyal/analyst/server/utils/QuotaLedgerTest.java
@@ -1,6 +1,8 @@
 package com.conveyal.analyst.server.utils;
 
+import com.google.common.io.Files;
 import junit.framework.TestCase;
+import models.Query;
 import models.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +25,7 @@ public class QuotaLedgerTest extends TestCase {
         ledger = new QuotaLedger(File.createTempFile("ledger", ".db").getAbsolutePath());
 
         user = new User("TEST", "TEST");
+        DataStore.dataPath = Files.createTempDir().getAbsolutePath();
     }
 
     /** Make sure that single point jobs are recorded and charged correctly */
@@ -82,6 +85,111 @@ public class QuotaLedgerTest extends TestCase {
         assertEquals(202, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).size());
         // make sure the balance is correct
         assertEquals(-198, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).get(201).balance);
+    }
+
+    @Test
+    public void testMultiPointPartialRefund () {
+        Query q = Query.create();
+        q.totalPoints = 100;
+        q.completePoints = 30;
+
+        // create a multipoint query
+        QuotaLedger.LedgerEntry e = new QuotaLedger.LedgerEntry();
+        e.delta = -100;
+        e.groupId = "TEST";
+        e.userId = "TEST";
+        e.query = q.id;
+        e.reason = QuotaLedger.LedgerReason.QUERY_CREATED;
+
+        ledger.add(e);
+
+        QuotaLedger.LedgerEntry partialRefund = ledger.refundQueryPartial(q, user);
+        assertEquals(e.id, partialRefund.parentId);
+        assertEquals(q.totalPoints - q.completePoints, partialRefund.delta);
+        assertEquals(q.id, e.query);
+        assertEquals(2, ledger.getEntriesForQuery(q.id).size());
+        assertEquals(-30, ledger.getValue("TEST"));
+        assertEquals(-30, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).get(1).balance);
+
+        // try to partially refund again
+        assertNull(ledger.refundQueryPartial(q, user));
+
+        // fully refund
+        QuotaLedger.LedgerEntry refund = ledger.refundQuery(q.id, user);
+        assertEquals(e.id, refund.parentId);
+        assertEquals((int) q.completePoints, refund.delta);
+        assertEquals(q.id, e.query);
+        assertEquals(3, ledger.getEntriesForQuery(q.id).size());
+        assertEquals(0, ledger.getValue("TEST"));
+        assertEquals(0, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).get(2).balance);
+    }
+
+    @Test
+    public void testMultipointFullRefund () {
+        Query q = Query.create();
+        q.totalPoints = 100;
+        q.completePoints = 30;
+
+        // create a multipoint query
+        QuotaLedger.LedgerEntry e = new QuotaLedger.LedgerEntry();
+        e.delta = -100;
+        e.groupId = "TEST";
+        e.userId = "TEST";
+        e.query = q.id;
+        e.reason = QuotaLedger.LedgerReason.QUERY_CREATED;
+
+        ledger.add(e);
+
+        // fully refund
+        QuotaLedger.LedgerEntry refund = ledger.refundQuery(q.id, user);
+        assertEquals(e.id, refund.parentId);
+        assertEquals((int) q.totalPoints, refund.delta);
+        assertEquals(q.id, e.query);
+        assertEquals(2, ledger.getEntriesForQuery(q.id).size());
+        assertEquals(0, ledger.getValue("TEST"));
+        assertEquals(0, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).get(1).balance);
+
+        // try to partially refund again
+        assertNull(ledger.refundQueryPartial(q, user));
+
+        // try to fully refund again
+        assertNull(ledger.refundQuery(q.id, user));
+
+        assertEquals(2, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).size());
+    }
+
+    @Test
+    public void testMultipleUsers () {
+        User u2 = new User("test2", "test2");
+
+        QuotaLedger.LedgerEntry e1 = new QuotaLedger.LedgerEntry();
+        e1.delta = -1;
+        e1.groupId = "TEST";
+        e1.userId = "TEST";
+        e1.reason = QuotaLedger.LedgerReason.SINGLE_POINT;
+        ledger.add(e1);
+
+        QuotaLedger.LedgerEntry e2 = new QuotaLedger.LedgerEntry();
+        e2.delta = 200;
+        e2.groupId = "test2";
+        e2.userId = "test2";
+        e2.reason = QuotaLedger.LedgerReason.PURCHASE;
+        ledger.add(e2);
+
+        QuotaLedger.LedgerEntry e3 = new QuotaLedger.LedgerEntry();
+        e3.delta = -1;
+        e3.groupId = "test2";
+        e3.userId = "test2";
+        e3.reason = QuotaLedger.LedgerReason.SINGLE_POINT;
+        ledger.add(e3);
+
+        assertEquals(-1, ledger.getValue("TEST"));
+        assertEquals(1, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).size());
+        assertEquals(-1, ledger.getLedgerEntries("TEST", 0, Long.MAX_VALUE).get(0).balance);
+
+        assertEquals(199, ledger.getValue("test2"));
+        assertEquals(2, ledger.getLedgerEntries("test2", 0, Long.MAX_VALUE).size());
+        assertEquals(199, ledger.getLedgerEntries("test2", 0, Long.MAX_VALUE).get(1).balance);
     }
 
     /** Create single point queries */
