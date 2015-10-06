2afc0cdbe429b78da53f7374a871099b3ef1d3af
Matthew Wigginton Conway <matt@indicatrix.org>
2015-10-06 08:32:46 -0400

    give broker more memory.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 # figure out how much memory to use. Stats get 512M, broker gets 1G, OS gets 1G, analyst gets rest
 TOTAL_MEM=`grep MemTotal /proc/meminfo | sed s/[^0-9]//g`
-ANALYST_MEM=`echo "$TOTAL_MEM - (2500 * 1024)" | bc`
+ANALYST_MEM=`echo "$TOTAL_MEM - (3500 * 1024)" | bc`
 
 # start analyst server, detaching it from the terminal
 # One would think that the nohup command should do this but it doesn't detach standard error
@@ -12,5 +12,5 @@ echo $! > /tmp/ANALYST_PID
 
 # start the broker. conveniently enough we can use the analyst server jar, which just happens to already have the
 # logentries client baked into it.
-java -Xmx1G -Dlogback.configurationFile=/etc/broker-logentries.xml -cp /opt/otp/analyst-server.jar org.opentripplanner.analyst.broker.BrokerMain /etc/broker.conf > /home/ubuntu/broker.log < /dev/null 2>&1 &
+java -Xmx2G -Dlogback.configurationFile=/etc/broker-logentries.xml -cp /opt/otp/analyst-server.jar org.opentripplanner.analyst.broker.BrokerMain /etc/broker.conf > /home/ubuntu/broker.log < /dev/null 2>&1 &
 echo $! > /tmp/BROKER_PID
