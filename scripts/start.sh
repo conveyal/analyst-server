a375b8f34c36517713f19359e97898b8044de89a
Matthew Wigginton Conway <matt@indicatrix.org>
2016-06-02 15:40:47 -0400

    Revert "give broker even more memory."
    This reverts commit 70cce113830d76d1a9256d89117245fab02c85e3.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 # figure out how much memory to use. Stats get 512M, broker gets 4G, OS gets 2G, analyst gets rest
 TOTAL_MEM=`grep MemTotal /proc/meminfo | sed s/[^0-9]//g`
-ANALYST_MEM=`echo "$TOTAL_MEM - (8500 * 1024)" | bc`
+ANALYST_MEM=`echo "$TOTAL_MEM - (6500 * 1024)" | bc`
 
 # start analyst server, detaching it from the terminal
 # One would think that the nohup command should do this but it doesn't detach standard error
@@ -12,6 +12,6 @@ java8 -Xmx${ANALYST_MEM}k -jar /opt/otp/analyst-server.jar /etc/analyst.conf > /
 echo $! > /home/ec2-user/ANALYST_PID
 
 # start the broker.
-java8 -Xmx6G -cp /opt/otp/analyst-server.jar com.conveyal.r5.analyst.broker.BrokerMain /etc/broker.conf > /home/ec2-user/broker.log < /dev/null 2>&1 &
+java8 -Xmx4G -cp /opt/otp/analyst-server.jar com.conveyal.r5.analyst.broker.BrokerMain /etc/broker.conf > /home/ec2-user/broker.log < /dev/null 2>&1 &
 echo $! > /home/ec2-user/BROKER_PID
 
